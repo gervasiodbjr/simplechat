65b21ca07903b519f288cc7ce6942bb5164bf1b5
gervasiodbjr@gmail.com <Gervsio D. Banderia Jr>
2018-04-20 18:08:31 -0300

    Implementations

--- app/models/usersDAO.js
+++ app/models/usersDAO.js
@@ -7,11 +7,28 @@ class userDAO {
 
     testLogin (email, password) {
         var SQL = "select * from users where mail = '" + email + "' and password = '" + password + "'";
-        var res = this._conn.execute(
-            SQL,
-            (err, result) => {
-                // return result[0].name;
-                console.log(result[0].name);
+        this.query(this._conn, SQL).then(
+            (res) => {
+                if (res.length === 1) {
+                    console.log(res[0])
+                } else {
+                    console.log('Login Fail')
+                }
+            }
+        )
+    }
+
+    query (conn, SQL) {
+        return new Promise(
+            (resolve, reject) => {
+                conn.connect()
+                conn.query(SQL , (err, rows) => {
+                    if (!err)
+                        resolve(rows);
+                    else
+                        reject(err);
+                })
+                conn.end();
             }
         )
     }
